09e289851dba9afffd0918f0443ca2166e5a3a50
JohnKamaujk <johnjkkamau@gmail.com>
2023-09-19 13:11:03 +0300

    :sparkles:chore:user module

--- models/city.py
+++ models/city.py
@@ -2,7 +2,7 @@
 """ City Module for HBNB project """
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, String
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 from models.place import Place
