3c94c0d36e7bf8f5fc4b11a0fb3ab998416f9287
pytbyte <heretolearn1@gmail.com>
2023-10-24 11:06:01 +0300

    Webflask updating 03

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -59,11 +59,7 @@ def number_template(n):
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_odd_or_even(n):
     """ Checks if int is odd or even and displays """
-    if n % 2 == 0:
-        msg = "odd"
-    else:
-        msg = "even"
-    return render_template("6-number_odd_or_even.html", n=n, msg=msg)
+    return render_template("6-number_odd_or_even.html", n=n)
 
 
 if __name__ == "__main__":
---
+++ web_flask/7-states_list.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Starts a Flask web application.
+
+listens on 0.0.0.0, port 5000.
+Routes:
+    /states_list: HTML page with a list of all States.
+"""
+from models import storage
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """
+    lists all states from the database.
+    """
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
